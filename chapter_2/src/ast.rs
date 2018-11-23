5fe7c73da2d75809124a56f8a1dd031c58e0ad68
maciejhirsz <maciej.hirsz@gmail.com>
2018-11-23 11:58:28 +0100

    Exercise for Chapter 2

--- chapter_2/src/ast.rs
+++ chapter_2/src/ast.rs
@@ -1,7 +1,9 @@
+#[derive(PartialEq, Debug, Clone)]
 pub struct Program {
-    pub statements: Vec<Expression>
+    pub body: Vec<Expression>
 }
 
+#[derive(PartialEq, Debug, Clone, Copy)]
 pub enum Operator {
     /// '+'
     Add,
@@ -26,27 +28,52 @@ pub enum Operator {
 // Ideally Rust would allow us to use things like
 // `Expression::BinaryExpression` as types, but we
 // are not there yet :).
+#[derive(PartialEq, Debug, Clone)]
 pub enum Expression {
-    Identifier(String),
-    Number(i32),
+    Identifier(Identifier),
+    Number(Number),
     UnaryExpression(UnaryExpression),
     BinaryExpression(BinaryExpression),
 }
 
+#[derive(PartialEq, Debug, Clone)]
+pub struct Identifier {
+    pub identifier: String,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Number {
+    pub number: u64,
+}
+
 // This is mostly useful for things like `-5`
+#[derive(PartialEq, Debug, Clone)]
 pub struct UnaryExpression {
-    operator: Operator,
-    operand: Box<Expression>,
+    pub operator: Operator,
+    pub operand: Box<Expression>,
 }
 
 // Should cover both assignment and addition
+#[derive(PartialEq, Debug, Clone)]
 pub struct BinaryExpression {
-    left: Box<Expression>,
-    operator: Operator,
-    right: Box<Expression>,
+    pub left: Box<Expression>,
+    pub operator: Operator,
+    pub right: Box<Expression>,
 }
 
 // Converters below
+impl From<Identifier> for Expression {
+    fn from(value: Identifier) -> Expression {
+        Expression::Identifier(value)
+    }
+}
+
+impl From<Number> for Expression {
+    fn from(value: Number) -> Expression {
+        Expression::Number(value)
+    }
+}
+
 impl From<UnaryExpression> for Expression {
     fn from(value: UnaryExpression) -> Expression {
         Expression::UnaryExpression(value)
--- chapter_2/src/lib.rs
+++ chapter_2/src/lib.rs
@@ -1,13 +1,184 @@
 mod ast;
 
-use ast::*;
+pub use ast::*;
 
 /// This struct needs some fields!
+///
+/// Try to incorporate the Lexer from the previous chapter inside here
 pub struct Parser;
 
 impl Parser {
     /// It also needs some code
     pub fn new(source: &str) -> Self {
         Parser
+    }
+
+    /// A bit of a helper that will improve ergonomics
+    pub fn parse<T: Parse>(&mut self) -> Result<T> {
+        T::parse(self)
+    }
+}
+
+/// Just a 0-size type for clarity, we could also use `Option` instead of `Result`
+#[derive(Debug)]
+pub struct ParseError;
+
+pub type Result<T> = ::std::result::Result<T, ParseError>;
+
+pub trait Parse: Sized {
+    fn parse(parser: &mut Parser) -> Result<Self>;
+}
+
+// All impls below need proper code!
+
+impl Parse for Program {
+    fn parse(parser: &mut Parser) -> Result<Program> {
+        Err(ParseError)
+    }
+}
+
+impl Parse for Identifier {
+    fn parse(parser: &mut Parser) -> Result<Identifier> {
+        Err(ParseError)
+    }
+}
+
+impl Parse for Number {
+    fn parse(parser: &mut Parser) -> Result<Number> {
+        Err(ParseError)
+    }
+}
+
+impl Parse for UnaryExpression {
+    fn parse(parser: &mut Parser) -> Result<UnaryExpression> {
+        Err(ParseError)
+    }
+}
+
+impl Parse for BinaryExpression {
+    fn parse(parser: &mut Parser) -> Result<BinaryExpression> {
+        Err(ParseError)
+    }
+}
+
+impl Parse for Expression {
+    fn parse(parser: &mut Parser) -> Result<Expression> {
+        // Suggested way to do this:
+        //
+        // match parser.lexer.token {
+        //     Token::_____ => ______::parse(parser),
+        //
+        //     ...
+        //
+        //     _ => Err(ParseError),
+        // }
+
+        Err(ParseError)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_identifier() {
+        let source = "foo;";
+
+        let ident: Identifier = Parser::new(source).parse().unwrap();
+
+        assert_eq!(ident, Identifier {
+            identifier: "foo".into(),
+        });
+    }
+
+    #[test]
+    fn parse_number() {
+        let source = "42;";
+
+        let num: Number = Parser::new(source).parse().unwrap();
+
+        assert_eq!(num, Number {
+            number: 42,
+        });
+    }
+
+    #[test]
+    fn parse_neg_number() {
+        let source = "-42;";
+
+        let unary: UnaryExpression = Parser::new(source).parse().unwrap();
+
+        assert_eq!(unary, UnaryExpression {
+            operator: Operator::Subtract,
+            operand: Box::new(Number {
+                number: 42,
+            }.into()),
+        });
+    }
+
+    #[test]
+    fn parse_binary_expression() {
+        let source = "5 * 3;";
+
+        let binary: BinaryExpression = Parser::new(source).parse().unwrap();
+
+        assert_eq!(binary, BinaryExpression {
+            left: Box::new(Number {
+                number: 5,
+            }.into()),
+            operator: Operator::Multiply,
+            right: Box::new(Number {
+                number: 3,
+            }.into()),
+        });
+    }
+
+    #[test]
+    fn all_together() {
+        let source = "foo = 42; bar = 10 * foo + foo / 3;";
+
+        let program: Program = Parser::new(source).parse().unwrap();
+
+        assert_eq!(program, Program {
+            body: vec![
+                BinaryExpression {
+                    left: Box::new(Identifier {
+                        identifier: "foo".into(),
+                    }.into()),
+                    operator: Operator::Assign,
+                    right: Box::new(Number {
+                        number: 42,
+                    }.into()),
+                }.into(),
+                BinaryExpression {
+                    left: Box::new(Identifier {
+                        identifier: "bar".into(),
+                    }.into()),
+                    operator: Operator::Assign,
+                    right: Box::new(BinaryExpression {
+                        left: Box::new(BinaryExpression {
+                            left: Box::new(Number {
+                                number: 10,
+                            }.into()),
+                            operator: Operator::Multiply,
+                            right: Box::new(Identifier {
+                                identifier: "foo".into(),
+                            }.into()),
+                        }.into()),
+                        operator: Operator::Add,
+                        right: Box::new(BinaryExpression {
+                            right: Box::new(Identifier {
+                                identifier: "foo".into(),
+                            }.into()),
+                            operator: Operator::Divide,
+                            left: Box::new(Number {
+                                number: 3,
+                            }.into()),
+                        }.into()),
+                    }.into())
+                }.into(),
+            ]
+        });
     }
 }
